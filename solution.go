6606db1c82ef4888b41ffb2b9e8663ca69498685
Anton Khozyainov <a.khozyainov@cherryhome.ai>
2022-02-22 21:26:42 +0300

    CalcSquare for triangle, square, circle

--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +10,23 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type sidesNumber int
+
+const (
+	SidesCircle   sidesNumber = 0
+	SidesTriangle sidesNumber = 3
+	SidesSquare   sidesNumber = 4
+)
+
+func CalcSquare(sideLen float64, sidesNum sidesNumber) float64 {
+	var square float64
+	switch sidesNum {
+	case SidesCircle:
+		square = math.Pi * sideLen * sideLen
+	case SidesTriangle:
+		square = sideLen * sideLen / 2
+	case SidesSquare:
+		square = sideLen * sideLen
+	}
+	return square
 }
