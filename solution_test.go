3c73e11f9646a362d4b8641af2a1e4a38208c5fe
Anton Khozyainov <a.khozyainov@cherryhome.ai>
2022-02-22 21:37:27 +0300

    fix tests quality

--- solution_test.go
+++ solution_test.go
@@ -5,23 +5,25 @@ import (
 	"testing"
 )
 
+const errorMsg = "Got %f, expected %f"
+
 func TestCalcSquare(t *testing.T) {
-	side_len := 10.0
-	triangle_expected := side_len * side_len * math.Sqrt(3) / 4
-	triangle_result := CalcSquare(side_len, SidesTriangle)
-	if triangle_result != triangle_expected {
-		t.Errorf("Got %f, expected %f", triangle_result, triangle_expected)
+	sideLen := 10.0
+	triangleExpected := sideLen * sideLen * math.Sqrt(3) / 4
+	triangleResult := CalcSquare(sideLen, SidesTriangle)
+	if triangleResult != triangleExpected {
+		t.Errorf(errorMsg, triangleResult, triangleExpected)
 	}
 
-	square_expected := side_len * side_len
-	square_result := CalcSquare(side_len, SidesSquare)
-	if square_result != square_expected {
-		t.Errorf("Got %f, expected %f", square_result, square_expected)
+	squareExpected := sideLen * sideLen
+	squareResult := CalcSquare(sideLen, SidesSquare)
+	if squareResult != squareExpected {
+		t.Errorf(errorMsg, squareResult, squareExpected)
 	}
 
-	circle_expected := side_len * side_len * math.Pi
-	circle_result := CalcSquare(side_len, SidesCircle)
-	if circle_result != circle_expected {
-		t.Errorf("Got %f, expected %f", circle_result, circle_expected)
+	circleExpected := sideLen * sideLen * math.Pi
+	circleResult := CalcSquare(sideLen, SidesCircle)
+	if circleResult != circleExpected {
+		t.Errorf(errorMsg, circleResult, circleExpected)
 	}
 }
